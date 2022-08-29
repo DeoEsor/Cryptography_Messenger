f98a8e0a66aadc9f84f9808f5195c5d9b9943edc
Valeriy Vartumyan <RulianValeriy@yandex.ru>
2022-08-29 11:59:14 +0300

    updated DI

--- CryptoDesktop/App.xaml.cs
+++ CryptoDesktop/App.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using CryptoDesktop.MVVM.ViewModel;
 using DryIoc;
-using СryptoClient;
 
 namespace CryptoDesktop
 {
@@ -24,7 +17,7 @@ namespace CryptoDesktop
 			Container = new Container();
 			
 			// Grpc client
-			Container.Register<CryptoClient>(Reuse.Singleton);
+			Container.Register<AuthClient>(Reuse.Singleton);
 			//Views
 			Container.Register<Registration>(Reuse.Transient);
 			Container.Register<MainWindow>(Reuse.Transient);
