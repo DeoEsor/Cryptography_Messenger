2a7e732a38de5a632404d6332aee64aafc35a4c6
Valeriy Vartumyan <vdvartumyan@mai.education>
2022-08-29 15:31:56 +0300

    Wiener Attack moved to static context

--- CryptographyLib/Attacks/WienerAttack.cs
+++ CryptographyLib/Attacks/WienerAttack.cs
@@ -3,32 +3,29 @@ using NumberTheory.Extensions;
 
 namespace CryptographyLib.Attacks;
 
-public class WienerAttack
+public static class WienerAttack
 {
+    private static readonly BigInteger M = new(0x01010101);
+    public static List<(BigInteger, BigInteger)> Attack(BigInteger isN, BigInteger isE) 
+    {
+        var limitD = isN.Sqrt().Sqrt() / 3;
+        
+        return BigIntegerExtensions
+            .CFRAC(isE, isN)
+            .GetConvergent()
+            .Where(s =>
+            {
+                if (s.Item2 > limitD)
+                    return false;
+                var c = BigInteger
+                    .ModPow(M, isE, isN);
+                
+                var m = BigInteger
+                    .ModPow(c, s.Item2, isN);
 
-        public List<(BigInteger, BigInteger)> Attack(BigInteger isN, BigInteger isE) 
-        {
-            var limitD = isN.Sqrt();
-            limitD = limitD.Sqrt();
-            limitD = limitD/3;
-            var _M = 0x01010101;
-            
-            return BigIntegerExtensions
-                .CFRAC(isE, isN)
-                .GetConvergent()
-                .Where(s =>
-                {
-                    if (s.Item2 > limitD)
-                        return false;
-                    var c = BigInteger
-                        .ModPow(_M, isE, isN);
-                    
-                    var m = BigInteger
-                        .ModPow(c, s.Item2, isN);
+                return M == m;
 
-                    return _M == m;
-
-                })
-                .ToList();
-        }
+            })
+            .ToList();
+    }
 }
