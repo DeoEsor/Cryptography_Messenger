b2276f0d190b5ef014e756f78fd679d943881113
DeoEsor <RulianValeriy@yandex.ru>
2022-09-19 14:20:50 +0300

    update

--- CryptographyLib.Tests/Symmetric/DesTest.cs
+++ CryptographyLib.Tests/Symmetric/DesTest.cs
@@ -5,27 +5,27 @@ using CryptographyLib.CipherModes;
 using CryptographyLib.KeyExpanders;
 using CryptographyLib.Paddings;
 using CryptographyLib.Symmetric;
-using CryptographyLib.Symmetric.AES;
 using NUnit.Framework;
 
-namespace CryptoTest.SymmetricAlgos;
+namespace CryptographyLib.Tests.Symmetric;
 
 [TestFixture]
-public class RijndaelTest
+public class DesTest
 {
     [Test]
-    public void Test()
+    public async Task Test()
     {
-        Random random = new Random();
-        //var generator = new FuncKeyGenerator<BigInteger>(new Func<BigInteger, byte[]>(s => s.ToByteArray()));
+        var random = new Random();
+
         var key = new byte[7];
+        
         TestContext.CurrentContext.Random.NextBytes(key);
         var expander = new DESExpander(key,new PKCS7());
         var context = new SymmetricEncryptorContext(CipherMode.Mode.ECB,
-            (ushort)random.Next(), new Rijndael(expander), 16);
+            (ushort)random.Next(), new DES(expander), 16);
         Debug.Write(Encoding.UTF8.GetString(key));
 
-        context.AsyncEncryptFile("Test.txt", "Encoded.txt");
-        context.AsyncDecryptFile("Encoded.txt", "Decoded.txt");
+        await context.AsyncEncryptFile("Test.txt", "Encoded.txt");
+        await context.AsyncDecryptFile("Encoded.txt", "Decoded.txt");
     }
 }
